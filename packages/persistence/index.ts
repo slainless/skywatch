47676f956aa3d6cf62d553f25f295a41867a9346
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2024-10-06 17:51:05 +0800

    change(persistence): removed lazy load from index, update exports

--- packages/persistence/index.ts
+++ packages/persistence/index.ts
@@ -1,5 +1,3 @@
 export type { KV } from "./kv.js";
 export type { Serializer } from "./serializer.js";
 export type { Persistence, GetResult } from "./persistence.js";
-
-export { LazyLoadPersistence } from "./lazy-load.js";
