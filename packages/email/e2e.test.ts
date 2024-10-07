196ba360a4be4ab9cd4e5c94eaa7485e870230ff
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2024-10-07 22:57:23 +0800

    test(email): added basic e2e test

--- packages/email/e2e.test.ts
+++ packages/email/e2e.test.ts
@@ -5,6 +5,7 @@ import { QUEUE_PREFIX } from "./broker";
 import { MessagePackSerializer } from "@deweazer/serializer";
 import ky from "ky";
 import { MailHog, RabbitMQ } from "./test/container";
+import { createEmailPusher, EmailPusher } from "./pusher";
 
 const mq = RabbitMQ().orchestrate();
 const mh = MailHog().orchestrate();
@@ -14,16 +15,21 @@ const mailhog = ky.create({ prefixUrl: "http://localhost:8025/api/v1" });
 const serializer = MessagePackSerializer.serializer;
 const createMail = <T extends SendMailOptions>(mail: T) => mail;
 
-beforeEach(() => mailhog.delete("messages"));
+beforeEach(async () => {
+	await mailhog.delete("messages");
+	await mq.channel.queueDelete(QUEUE_PREFIX + email);
+});
 
-describe(EmailSender.name, () => {
-	beforeEach(async () => {
-		mock.restore();
-		await mq.channel.queueDelete(QUEUE_PREFIX + email);
-	});
+const email = "noreply@localhost";
+const mail = createMail({
+	from: email,
+	to: "aiman.fauzy@localhost",
+	subject: "Hello, World!",
+	text: "😁😁😁😁リムルは真の竜種に進化した🗿💀😱🤦‍♂️🌖🤣",
+});
 
-	const email = "noreply@localhost";
-	it("should be able to consume queue message and send it as email", async () => {
+describe("E2E on same process", () => {
+	it("should be able to proceed with same channel", async () => {
 		const transport = createTransport({
 			port: 1025,
 			host: "localhost",
@@ -31,27 +37,12 @@ describe(EmailSender.name, () => {
 			ignoreTLS: true,
 		});
 
-		const queue = await mq.channel.queue(QUEUE_PREFIX + email);
+		const pusher = await createEmailPusher(mq.channel, email);
 		const sender = await createEmailSender(mq.channel, email, transport);
 
-		const subscribeSpy = spyOn(sender.queue, "subscribe");
-		expect(subscribeSpy).toHaveBeenCalledTimes(0);
 		await sender.startSending();
-		expect(subscribeSpy).toHaveBeenCalledTimes(1);
-
-		const sendMailSpy = spyOn(transport, "sendMail");
-		expect(sendMailSpy).toHaveBeenCalledTimes(0);
-
-		const mail = createMail({
-			from: email,
-			to: "aiman.fauzy@localhost",
-			subject: "Hello, World!",
-			text: "😁😁😁😁リムルは真の竜種に進化した🗿💀😱🤦‍♂️🌖🤣",
-		});
-		await queue.publish(serializer.serialize(mail));
-		await Bun.sleep(100);
-		expect(sendMailSpy).toHaveBeenCalledTimes(1);
-		expect(sendMailSpy.mock.lastCall).toEqual([mail]);
+		await pusher.send(mail);
+		await Bun.sleep(300);
 
 		const messages = await mailhog.get("messages").json<any>();
 		expect(messages).toBeArrayOfSize(1);
@@ -61,10 +52,8 @@ describe(EmailSender.name, () => {
 		expect(`${got.To[0].Mailbox}@${got.To[0].Domain}`).toBe(mail.to);
 		expect(Buffer.from(got.Content.Body, "base64").toString()).toBe(mail.text);
 	});
-});
 
-describe("Local mail simulation with mailhog", () => {
-	it("should allow sending and receiving from/to local server", async () => {
+	it("should be able to proceed with different channel", async () => {
 		const transport = createTransport({
 			port: 1025,
 			host: "localhost",
@@ -72,13 +61,15 @@ describe("Local mail simulation with mailhog", () => {
 			ignoreTLS: true,
 		});
 
-		const mail = createMail({
-			from: "noreply@localhost",
-			to: "ahmad.fauzy@localhost",
-			subject: "Hello, World!",
-			text: "ちなみに、私はArchを使っています。",
-		});
-		await transport.sendMail(mail);
+		const pushChannel = await mq.client.channel(2);
+		const pullChannel = await mq.client.channel(3);
+
+		const pusher = await createEmailPusher(pushChannel, email);
+		const sender = await createEmailSender(pullChannel, email, transport);
+
+		await sender.startSending();
+		await pusher.send(mail);
+		await Bun.sleep(300);
 
 		const messages = await mailhog.get("messages").json<any>();
 		expect(messages).toBeArrayOfSize(1);
