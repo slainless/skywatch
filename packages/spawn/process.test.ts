91a7d4778085719994dfe3ec7daace9a56360847
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2024-10-13 17:24:24 +0800

    test(spawn): fixture spawn directory to the module

--- packages/spawn/process.test.ts
+++ packages/spawn/process.test.ts
@@ -1,18 +1,23 @@
 import { describe, expect, it } from "bun:test";
 import { AbortError, createProcess } from "./process.js";
 
+const basePath = import.meta.url;
+
 describe(createProcess.name, () => {
 	it("Should reject when returning early", async () => {
 		try {
-			const process = await createProcess("bun ./test/hello.sh", (line) => {
-				return false;
-			});
+			const process = await createProcess(
+				`bun ${Bun.fileURLToPath(new URL("test/hello.sh", basePath))}`,
+				(line) => {
+					return false;
+				},
+			);
 			expect().fail("function should throw");
 		} catch (e) {
 			expect(e).toBeInstanceOf(AbortError);
 			if (e instanceof AbortError === false)
 				return expect().fail(`Not ${AbortError.name}`);
-			expect(e.reason).toBe("Spawned process exiting early");
+			expect(e.reason).toBe("Spawned process exiting early with code: 0");
 		}
 	});
 
@@ -39,12 +44,15 @@ describe(createProcess.name, () => {
 		][Symbol.iterator]();
 
 		try {
-			const process = await createProcess("bun ./test/sleep.sh", (lines) => {
-				for (const line of lines.trim().split("\n"))
-					expect(line).toBe(lineSequences.next().value as string);
+			const process = await createProcess(
+				`bun ${Bun.fileURLToPath(new URL("test/sleep.sh", basePath))}`,
+				(lines) => {
+					for (const line of lines.trim().split("\n"))
+						expect(line).toBe(lineSequences.next().value as string);
 
-				return lines.indexOf("Service is ready!") !== -1;
-			});
+					return lines.indexOf("Service is ready!") !== -1;
+				},
+			);
 			expect(process.killed).toBe(false);
 		} catch (e) {
 			console.error(e);
@@ -62,12 +70,15 @@ describe(createProcess.name, () => {
 		][Symbol.iterator]();
 
 		try {
-			const process = await createProcess("bun ./test/sleep.sh", (lines) => {
-				for (const line of lines.trim().split("\n"))
-					expect(line).toBe(lineSequences.next().value as string);
+			const process = await createProcess(
+				`bun ${Bun.fileURLToPath(new URL("test/sleep.sh", basePath))}`,
+				(lines) => {
+					for (const line of lines.trim().split("\n"))
+						expect(line).toBe(lineSequences.next().value as string);
 
-				return lines.indexOf("Service is WHAT") !== -1;
-			});
+					return lines.indexOf("Service is WHAT") !== -1;
+				},
+			);
 			expect().fail();
 		} catch (e) {
 			expect(e).toBeInstanceOf(AbortError);
@@ -86,12 +97,15 @@ describe(createProcess.name, () => {
 		][Symbol.iterator]();
 
 		try {
-			const process = await createProcess("bun ./test/sleep.sh", (lines) => {
-				for (const line of lines.trim().split("\n"))
-					expect(line).toBe(lineSequences.next().value as string);
+			const process = await createProcess(
+				`bun ${Bun.fileURLToPath(new URL("test/sleep.sh", basePath))}`,
+				(lines) => {
+					for (const line of lines.trim().split("\n"))
+						expect(line).toBe(lineSequences.next().value as string);
 
-				return lines.indexOf("Service is ready!") !== -1;
-			});
+					return lines.indexOf("Service is ready!") !== -1;
+				},
+			);
 			expect(process.killed).toBe(false);
 			const reader = process.stdout.getReader();
 			expect(reader).toBeInstanceOf(ReadableStreamDefaultReader);
