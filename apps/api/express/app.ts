926209c2843ae035742a0c63a11d8d2f9a6f593d
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2024-10-13 13:59:11 +0800

    feat(api): custom error handler

--- apps/api/express/app.ts
+++ apps/api/express/app.ts
@@ -3,6 +3,7 @@ import express from "express";
 import type { CacheMetadataRepository } from "../repository/cache-metadata";
 import type { WeatherService } from "../service/weather";
 import { WeatherController } from "./v1/weather";
+import { ErrorHandler } from "./v1/error";
 
 export function createServer(
 	weatherService: WeatherService,
@@ -10,11 +11,13 @@ export function createServer(
 ): Express {
 	const app = express();
 
+	const error = new ErrorHandler();
 	const weather = new WeatherController(weatherService, weatherCache);
 
 	const v1 = express.Router();
 	v1.get("/weathers", weather.mount());
 
 	app.get("/v1", v1);
+	app.use(error.mount());
 	return app;
 }
---
+++ apps/api/express/v1/error.ts
@@ -0,0 +1,14 @@
+import { Backend, HTTPError } from "@skywatch/common";
+import type { ErrorRequestHandler } from "express";
+
+export class ErrorHandler extends Backend.Component {
+	mount(): ErrorRequestHandler {
+		return (error, req, res, next) => {
+			this.logger?.error(error);
+			if (error instanceof HTTPError)
+				res.status(error.status).send(error.message);
+			else if (error instanceof Error) res.status(500).send(error.message);
+			else res.status(500).send(error);
+		};
+	}
+}
