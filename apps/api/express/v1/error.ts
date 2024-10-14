fbb8aa9994703e4e28da0f1512d9953064dd26ab
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2024-10-14 17:41:18 +0800

    change(api): set content type to text/plain

--- apps/api/express/v1/error.ts
+++ apps/api/express/v1/error.ts
@@ -5,6 +5,7 @@ export class ErrorHandler extends Backend.Component {
   mount(): ErrorRequestHandler {
     return (error, req, res, next) => {
       this.logger?.error(error);
+      res.set("Content-Type", "text/plain; charset=utf-8");
       if (error instanceof HTTPError)
         res.status(error.status).send(error.message);
       else if (error instanceof Error) res.status(500).send(error.message);
