7fbfcd73000c096fd6c018035f6d4591dab0ebbd
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2024-10-20 18:34:46 +0800

    chore(front): setup unplugin-icons

--- apps/front/vite.config.js
+++ apps/front/vite.config.js
@@ -1,10 +1,12 @@
-import { vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
-import paths from "vite-tsconfig-paths";
+import { vitePlugin as remix } from "@remix-run/dev"
+import { defineConfig } from "vite"
+import paths from "vite-tsconfig-paths"
+import icons from 'unplugin-icons/vite'
 
 export default defineConfig({
   plugins: [
     process.IS_STORYBOOK ? [] : [remix()],
     paths({ ignoreConfigErrors: true }),
+    icons({ compiler: 'jsx', jsx: 'react' })
   ],
-});
+})
